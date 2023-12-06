0400d335a1991554720dd2bb0f951b86c0e73759
Violeta1110 <149258879+Violeta1110@users.noreply.github.com>
2023-12-06 14:38:28 +0100

    Homework

---
+++ Homework 1/script.js
@@ -0,0 +1,19 @@
+// alert('it works')
+
+let onePhone = 119.95;
+let taxRate = 0.05;
+let numOfPhones = 30;
+let totalPriceOfPhones = onePhone * numOfPhones;
+let taxOf30Phones = totalPriceOfPhones * taxRate;
+let totalPriceWithTax = taxOf30Phones + totalPriceOfPhones;
+
+console.log('Price of one phone:$', onePhone);
+console.log('Tax rate:', taxRate);
+console.log('Number of phones:', numOfPhones);
+console.log('Total price of phones:$', totalPriceOfPhones);
+console.log('Tax of 30 phones:$', taxOf30Phones);
+console.log('Total price with tax:$', totalPriceWithTax);
+
+
+
+
